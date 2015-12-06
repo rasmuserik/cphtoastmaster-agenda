8da84b3eea0f962672e0db37e2672cd2f3afec3c
Rasmus Erik Voel Jensen <github-rasmuserik@solsort.dk>
2015-12-06 17:52:18 +0100

    fix html + cors-experiment

--- src/app/hello.cljs
+++ src/app/hello.cljs
@@ -1,3 +1,13 @@
-(ns app.hello) ; #
+(ns app.hello
+  (:require-macros  [cljs.core.async.macros :refer  [go go-loop alt!]])
+  (:require
+    [solsort.util :refer  [route log]]
+    [reagent.core :as reagent :refer  []]
+    [solsort.util :refer  [<ajax]]
+    [cljs.core.async :refer  [>! <! chan put! take! timeout close! pipe]])
 
-(js/console.log "hello")
+  )
+
+(go
+  (js/console.log (<! (<ajax "//solsort.com/cors/http/tmclub.eu/portal.php?page=1&c=381" :result "text")))
+  )
